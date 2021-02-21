8e7d20c8001abe63537b5e81e3dcf074c7eda0f9
Balintus00 <traxlerbalint@gmail.com>
2021-02-21 15:51:29 +0100

    Basics refactor #1

--- Application/app/src/main/java/hu/bme/aut/android/together/features/shared/activity/MainActivity.kt
+++ Application/app/src/main/java/hu/bme/aut/android/together/features/shared/activity/MainActivity.kt
@@ -1,12 +1,11 @@
-package hu.bme.aut.android.together
+package hu.bme.aut.android.together.features.shared.activity
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import hu.bme.aut.android.together.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,11 +15,6 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
