ae008ad97bbf2d980a01e087937d309da8afcdc9
Balintus00 <traxlerbalint@gmail.com>
2021-03-01 00:34:31 +0100

    After merge cleanup + Add Event Feature connected by navigation

--- Application/app/src/main/java/hu/bme/aut/android/together/features/currentevents/fragment/ComingEventListFragment.kt
+++ Application/app/src/main/java/hu/bme/aut/android/together/features/currentevents/fragment/ComingEventListFragment.kt
@@ -1,11 +1,15 @@
 package hu.bme.aut.android.together.features.currentevents.fragment
 
+import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_current_event_list.*
 
 class ComingEventListFragment : CurrentEventListFragment() {
     override fun setFabBehaviour() {
-        eventListAddFab.setOnClickListener{
-            // TODO navigating to event creation view
+        eventListAddFab.setOnClickListener {
+            CurrentEventsListsContainerFragmentDirections.actionCurrentEventsListFragmentToNameAdderFragment()
+                .let { action ->
+                    findNavController().navigate(action)
+                }
         }
     }
 }
