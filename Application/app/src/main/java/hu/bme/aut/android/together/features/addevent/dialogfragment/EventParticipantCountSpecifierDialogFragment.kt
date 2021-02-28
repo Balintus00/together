5b9c827fbead6097e2358f2af644df422ba26961
Balintus00 <traxlerbalint@gmail.com>
2021-02-28 11:11:31 +0100

    Public Event Details changes
    - Fixed typo in the request auto accpept options
    - Added new DialogButton styles (positive, negative); previous DialogButton style should be used as base of concrete DialogButton styles
    - Added title to PublicEventParticipantQuantifierFragment
    - The Max Participant count quantifier DialogFragment's buttons are actual buttons from now on
    - EventParticipantCountSpecifierDialogFragment behaviour changed

--- Application/app/src/main/java/hu/bme/aut/android/together/features/addevent/dialogfragment/EventParticipantCountSpecifierDialogFragment.kt
+++ Application/app/src/main/java/hu/bme/aut/android/together/features/addevent/dialogfragment/EventParticipantCountSpecifierDialogFragment.kt
@@ -1,5 +1,6 @@
 package hu.bme.aut.android.together.features.addevent.dialogfragment
 
+import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.text.Editable
@@ -9,11 +10,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
+import hu.bme.aut.android.together.R
 import hu.bme.aut.android.together.databinding.DialogfragmentEventParticipantCountSpecifierBinding
 
 class EventParticipantCountSpecifierDialogFragment : DialogFragment() {
 
     private lateinit var binding: DialogfragmentEventParticipantCountSpecifierBinding
+
+    private var isCountSpecified = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setDialogStyle()
+    }
+
+    private fun setDialogStyle() {
+        setStyle(STYLE_NORMAL, R.style.DialogWithTitle)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,20 +38,26 @@ class EventParticipantCountSpecifierDialogFragment : DialogFragment() {
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpDialogFragmentUI()
+    }
+
+    private fun setUpDialogFragmentUI(){
+        dialog?.let{
+            setDialogAttributes(it)
+        }
         setWidgetsBehaviour()
     }
 
-    override fun onDismiss(dialog: DialogInterface) {
-        super.onDismiss(dialog)
-        findNavController().popBackStack()
+    private fun setDialogAttributes(dialog: Dialog){
+        dialog.setTitle(getString(R.string.title_event_participant_count_dialogfragment))
     }
 
     private fun setWidgetsBehaviour() {
         setEditTextBehaviour()
-        setApplyTextViewBehaviour()
+        setApplyButtonBehaviour()
+        setCancelButtonBehaviour()
     }
 
     private fun setEditTextBehaviour() {
@@ -55,13 +74,26 @@ class EventParticipantCountSpecifierDialogFragment : DialogFragment() {
         })
     }
 
-    private fun setApplyTextViewBehaviour() {
-        binding.tvApplyParticipantCount.setOnClickListener {
+    private fun setApplyButtonBehaviour() {
+        binding.btnApplyParticipantCount.setOnClickListener {
             // TODO: passing data using ViewModel probably.
             //  Also the passed data's validness should be checked.
             //  If something is wrong, the user can be notified here about it.
+            isCountSpecified = true
             dismiss()
         }
+    }
+
+    private fun setCancelButtonBehaviour() {
+        binding.btnCancelParticipantCount.setOnClickListener{
+            dismiss()
+        }
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        if(isCountSpecified)
+            findNavController().popBackStack()
     }
 
 }
