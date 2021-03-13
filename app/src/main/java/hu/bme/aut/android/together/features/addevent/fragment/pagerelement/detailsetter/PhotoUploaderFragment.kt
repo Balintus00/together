01cbe5f9bdfc739b06176c0893d96a3dcfd43a17
Balintus00 <traxlerbalint@gmail.com>
2021-03-13 19:46:52 +0100

    Fixed gradlew build error
    - Imported androidx.fragment properly to use ActivityContractResults API
    - Also updated gradle to 6.8.3

--- app/src/main/java/hu/bme/aut/android/together/features/addevent/fragment/pagerelement/detailsetter/PhotoUploaderFragment.kt
+++ app/src/main/java/hu/bme/aut/android/together/features/addevent/fragment/pagerelement/detailsetter/PhotoUploaderFragment.kt
@@ -9,12 +9,12 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 import hu.bme.aut.android.together.R
 import hu.bme.aut.android.together.databinding.FragmentPhotoUploaderBinding
