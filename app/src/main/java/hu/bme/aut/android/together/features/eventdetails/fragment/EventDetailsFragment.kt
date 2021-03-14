7290a2ff0eb4fbf0e36e6ca5fbf9624ff6545157
Balintus00 <traxlerbalint@gmail.com>
2021-03-14 23:07:45 +0100

    EventDetailsFragment initial implementation
    - The MapView hasn't been added yet

--- app/src/main/java/hu/bme/aut/android/together/features/eventdetails/fragment/EventDetailsFragment.kt
+++ app/src/main/java/hu/bme/aut/android/together/features/eventdetails/fragment/EventDetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import hu.bme.aut.android.together.R
 import hu.bme.aut.android.together.databinding.FragmentEventDetailsBinding
 
@@ -18,5 +19,30 @@ class EventDetailsFragment : Fragment() {
     ): View {
         binding = FragmentEventDetailsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpUiWidgets()
+    }
+
+    private fun setUpUiWidgets() {
+        setUpToolbar()
+        setShowWholeDescriptionTextBehaviour()
+    }
+
+    private fun setUpToolbar() {
+        with(binding.tbEventDetails){
+            setNavigationIcon(R.drawable.ic_action_arrow_back)
+            setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    private fun setShowWholeDescriptionTextBehaviour() {
+        binding.tvShowMoreInformationEventDetails.setOnClickListener {
+            //TODO
+        }
     }
 }
---
+++ app/src/main/java/hu/bme/aut/android/together/features/eventdetails/fragment/EventDetailsFullDescriptionFragment.kt
@@ -0,0 +1,21 @@
+package hu.bme.aut.android.together.features.eventdetails.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import hu.bme.aut.android.together.R
+
+class EventDetailsFullDescriptionFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event_details_full_description, container, false)
+    }
+}
